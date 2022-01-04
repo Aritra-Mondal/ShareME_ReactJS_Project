4cae32ed06462237021138e30e8290db2a78bc1c
Aritra-Mondal <aritra.mondal.0538@gmail.com>
2022-01-04 16:01:15 +0530

    Start creating pin

---
+++ shareme_frontend/src/components/CreatePin.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import { MdDelete } from "react-icons/md";
+
+import { categories } from "../utils/data";
+import { client } from "../client";
+import Spinner from "./Spinner";
+
+function CreatePin({ user }) {
+  return <div>CreatePin</div>;
+}
+
+export default CreatePin;
